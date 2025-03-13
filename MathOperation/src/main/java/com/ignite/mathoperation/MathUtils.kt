7bf5bccfe69a259359a1cfb89ebf1f9623599e0c
anhquangtech <duonganhquang97@gmail.com>
2025-03-13 10:44:33 +0700

    first commit

---
+++ MathOperation/src/main/java/com/ignite/mathoperation/MathUtils.kt
@@ -0,0 +1,5 @@
+package com.ignite.mathoperation
+
+fun performAdd(a: Int, b: Int): Int {
+    return  a + b
+}
---
+++ app/src/main/java/com/ignite/androidresearch/AccessMathUtils.kt
@@ -0,0 +1,7 @@
+package com.ignite.androidresearch
+
+import com.ignite.mathoperation.performAdd
+
+fun main() {
+    println(performAdd(10,20))
+}
